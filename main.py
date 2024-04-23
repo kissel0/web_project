cd1bda3e22639d558138c9ebe2b50bd10e0d2651
Iriiin <151850130+Iriiin@users.noreply.github.com>
2024-04-23 16:50:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,86 @@
 import requests
-from translate import Translator
 
 
-# Функция для получения информации о стране через API OpenTripMap
-def get_country_info(country_name):
-    url = f"https://api.opentripmap.com/0.1/en/places/geoname?name={country_name}&apikey=5ae2e3f221c38a28845f05b680412c05b68a17d373dabe83dffb5b7a"
+def get_culture_info(lat, lon):
+    culture_info = []
+    url = f"https://api.geoapify.com/v2/places?categories=entertainment.culture&filter=circle:{lon},{lat},500000&bias=proximity:{lon},{lat}&limit=20&apiKey=23b70e7a81254e459c88d574598a37ab"
+    response = requests.get(url).json()
+    for i in response['features']:
+        try:
+            x = i['properties']['name']
+            culture_info.append(x)
+        except:
+            pass
+    return culture_info
+
+
+def get_nuturel_info(lat, lon):
+    f = []
+    cat = [('entertainment.zoo', 3), ('entertainment.planetarium', 3), ('natural', 10)]
+    for i in cat:
+        url = f"https://api.geoapify.com/v2/places?categories={i[0]}&filter=circle:{lon},{lat},500000&bias=proximity:{lon},{lat}&limit={i[-1]}&apiKey=23b70e7a81254e459c88d574598a37ab"
+        response = requests.get(url)
+        info = response.json()
+        for j in info['features']:
+            try:
+                x = j['properties']['name']
+                f.append(x)
+            except:
+                pass
+    return f
+
+
+def get_food_info(lat, lon):
+    food_info = []
+    url = f"https://api.geoapify.com/v2/places?categories=catering.restaurant&filter=circle:{lon},{lat},500000&bias=proximity:{lon},{lat}&limit=20&apiKey=23b70e7a81254e459c88d574598a37ab"
     response = requests.get(url)
-    lat, lon = response.json()['lat'], response.json()['lon']
-    print(lat, lon)
-    url_2 = f"https://api.geoapify.com/v2/places?categories=entertainment&filter=circle:{int(lon)},{int(lat)},9000000000000000000000000000000000000000000000000000000000&lang=en&limit=20&apiKey=23b70e7a81254e459c88d574598a37ab"
+    for i in response.json()['features']:
+        try:
+            x = i['properties']['name']
+            food_info.append(x)
+        except:
+            pass
+    url_2 = f"https://api.geoapify.com/v2/places?categories=catering.fast_food&filter=circle:{lon},{lat},500000&bias=proximity:{lon},{lat}&limit=20&apiKey=23b70e7a81254e459c88d574598a37ab"
     response_2 = requests.get(url_2)
-    country_info = response_2.json()
-    return country_info
+    for j in response_2.json()['features']:
+        try:
+            x = j['properties']['name']
+            food_info.append(x)
+        except:
+            pass
+    url_3 = f"https://api.geoapify.com/v2/places?categories=catering.cafe&filter=circle:{lon},{lat},500000&bias=proximity:{lon},{lat}&limit=20&apiKey=23b70e7a81254e459c88d574598a37ab"
+    response_3 = requests.get(url_3)
+    for l in response_3.json()['features']:
+        try:
+            x = l['properties']['name']
+            food_info.append(x)
+        except:
+            pass
+    return food_info
 
 
-# Функция для получения цен на билеты через API Flight Data
-def get_flight_prices(from_city, to_city):
-    url = f"https://api.flightdata.com/prices?from={from_city}&to={to_city}&apikey=661407AB679CC2D71F02C9B5"
+def get_entertainment_info(lat, lon):
+    entertainment_info = []
+    url = f"https://api.geoapify.com/v2/places?categories=entertainment.cinema&filter=circle:{lon},{lat},500000&bias=proximity:{lon},{lat}&limit=20&apiKey=23b70e7a81254e459c88d574598a37ab"
     response = requests.get(url)
-    flight_prices = response.json()
-    return flight_prices
+    for j in response.json()['features']:
+        try:
+            x = j['properties']['name']
+            entertainment_info.append(x)
+        except:
+            pass
+    url_2 = f"https://api.geoapify.com/v2/places?categories=entertainment.water_park&filter=circle:{lon},{lat},500000&bias=proximity:{lon},{lat}&limit=20&apiKey=23b70e7a81254e459c88d574598a37ab"
+    response_2 = requests.get(url_2)
+    for i in response_2.json()['features']:
+        try:
+            x = i['properties']['name']
+            entertainment_info.append(x)
+        except:
+            pass
+    return entertainment_info
 
 
-country_info = get_country_info("Rim")
-for i in country_info['features']:
-    print(i['properties']['name'])
+print(get_entertainment_info(59.85367598, 30.152849675))
+print(get_culture_info(59.85367598, 30.152849675))
+print(get_food_info(59.85367598585305, 30.1528496750455))
+print(get_nuturel_info(59.85367598585305, 30.1528496750455))
