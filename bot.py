8cd40adb5d8e32e4980756f9549c187d13efa9a8
Iriiin <151850130+Iriiin@users.noreply.github.com>
2024-04-26 19:02:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,85 +23,81 @@ markup1 = ReplyKeyboardMarkup(reply_keyboard1, one_time_keyboard=True)
 
 m = []
 
+import requests
+
 
 def get_culture_info(lat, lon):
-    culture_info = []
-    url = f"https://api.geoapify.com/v2/places?categories=entertainment.culture&filter=circle:{lon},{lat},500000&bias=proximity:{lon},{lat}&limit=20&apiKey=23b70e7a81254e459c88d574598a37ab"
+    culture_info = set()
+    url = f"https://api.geoapify.com/v2/places?categories=entertainment.culture&filter=circle:{lon},{lat},50000&bias=proximity:{lon},{lat}&limit=20&apiKey=23b70e7a81254e459c88d574598a37ab"
     response = requests.get(url).json()
     for i in response['features']:
         try:
-            x = i['properties']['name']
-            culture_info.append(x)
+            culture_info.add((i['properties']['name'], i['properties']['website']))
         except:
             pass
     return culture_info
 
 
 def get_nuturel_info(lat, lon):
-    f = []
+    f = set()
     cat = [('entertainment.zoo', 3), ('entertainment.planetarium', 3), ('natural', 10)]
     for i in cat:
-        url = f"https://api.geoapify.com/v2/places?categories={i[0]}&filter=circle:{lon},{lat},500000&bias=proximity:{lon},{lat}&limit={i[-1]}&apiKey=23b70e7a81254e459c88d574598a37ab"
+        url = f"https://api.geoapify.com/v2/places?categories={i[0]}&filter=circle:{lon},{lat},50000&bias=proximity:{lon},{lat}&limit={i[-1]}&apiKey=23b70e7a81254e459c88d574598a37ab"
         response = requests.get(url)
         info = response.json()
         for j in info['features']:
             try:
-                x = j['properties']['name']
-                f.append(x)
+                f.add((j['properties']['name'], j['properties']['website']))
             except:
                 pass
     return f
 
 
 def get_food_info(lat, lon):
-    food_info = []
-    url = f"https://api.geoapify.com/v2/places?categories=catering.restaurant&filter=circle:{lon},{lat},500000&bias=proximity:{lon},{lat}&limit=20&apiKey=23b70e7a81254e459c88d574598a37ab"
+    food_info = set()
+    url = f"https://api.geoapify.com/v2/places?categories=catering.restaurant&filter=circle:{lon},{lat},50000&bias=proximity:{lon},{lat}&limit=20&apiKey=23b70e7a81254e459c88d574598a37ab"
     response = requests.get(url)
     for i in response.json()['features']:
         try:
-            x = i['properties']['name']
-            food_info.append(x)
+            food_info.add((i['properties']['name'], i['properties']['website']))
         except:
             pass
-    url_2 = f"https://api.geoapify.com/v2/places?categories=catering.fast_food&filter=circle:{lon},{lat},500000&bias=proximity:{lon},{lat}&limit=20&apiKey=23b70e7a81254e459c88d574598a37ab"
+    url_2 = f"https://api.geoapify.com/v2/places?categories=catering.fast_food&filter=circle:{lon},{lat},50000&bias=proximity:{lon},{lat}&limit=20&apiKey=23b70e7a81254e459c88d574598a37ab"
     response_2 = requests.get(url_2)
     for j in response_2.json()['features']:
         try:
-            x = j['properties']['name']
-            food_info.append(x)
+            food_info.add((j['properties']['name'], j['properties']['website']))
         except:
             pass
-    url_3 = f"https://api.geoapify.com/v2/places?categories=catering.cafe&filter=circle:{lon},{lat},500000&bias=proximity:{lon},{lat}&limit=20&apiKey=23b70e7a81254e459c88d574598a37ab"
+    url_3 = f"https://api.geoapify.com/v2/places?categories=catering.cafe&filter=circle:{lon},{lat},50000&bias=proximity:{lon},{lat}&limit=20&apiKey=23b70e7a81254e459c88d574598a37ab"
     response_3 = requests.get(url_3)
     for l in response_3.json()['features']:
         try:
-            x = l['properties']['name']
-            food_info.append(x)
+            food_info.add((l['properties']['name'], l['properties']['website']))
         except:
             pass
     return food_info
 
 
 def get_entertainment_info(lat, lon):
-    entertainment_info = []
-    url = f"https://api.geoapify.com/v2/places?categories=entertainment.cinema&filter=circle:{lon},{lat},500000&bias=proximity:{lon},{lat}&limit=20&apiKey=23b70e7a81254e459c88d574598a37ab"
+    entertainment_info = set()
+    url = f"https://api.geoapify.com/v2/places?categories=entertainment.cinema&filter=circle:{lon},{lat},50000&bias=proximity:{lon},{lat}&limit=20&apiKey=23b70e7a81254e459c88d574598a37ab"
     response = requests.get(url)
     for j in response.json()['features']:
         try:
-            x = j['properties']['name']
-            entertainment_info.append(x)
+            entertainment_info.add((j['properties']['name'], j['properties']['website']))
         except:
             pass
-    url_2 = f"https://api.geoapify.com/v2/places?categories=entertainment.water_park&filter=circle:{lon},{lat},500000&bias=proximity:{lon},{lat}&limit=20&apiKey=23b70e7a81254e459c88d574598a37ab"
+    url_2 = f"https://api.geoapify.com/v2/places?categories=entertainment.water_park&filter=circle:{lon},{lat},50000&bias=proximity:{lon},{lat}&limit=20&apiKey=23b70e7a81254e459c88d574598a37ab"
     response_2 = requests.get(url_2)
     for i in response_2.json()['features']:
         try:
-            x = i['properties']['name']
-            entertainment_info.append(x)
+            entertainment_info.add((i['properties']['name'], i['properties']['website']))
+
         except:
             pass
+    print(entertainment_info)
     return entertainment_info
-
 
 
 async def start(update, context):
@@ -116,12 +112,11 @@ async def start(update, context):
 
 async def receive_location(update, context):
     locat = update.message.location
-    print(locat)
+    context.user_data['locality'] = update.message.text
     loc = str(locat).lstrip('Location').strip('()').split('=')
     del loc[0]
     l = ' '.join(loc).split(', longitude ')
-    m.append(float(l[0]))
-    m.append(float(l[1]))
+    context.user_data['locality'] = (float(l[0]), float(l[1]))
     await update.message.reply_text("Мы нашли ближайшие к вам места, в которые вы можете сходить.\n"
                                     "Выберите пожалуйста категорию места",
                                     reply_markup=markup1)
@@ -132,24 +127,27 @@ async def second_response(update, context):
     # Ответ на второй вопрос.
     # Мы можем его сохранить в базе данных или переслать куда-либо.
     weather = update.message.text
+    locat = context.user_data['locality']
     flag = True
     if weather == 'развлечения':
-        entert_lst = list(get_entertainment_info(m[0], m[1]))
+        entert_lst = list(get_entertainment_info(locat[0], locat[-1]))
         await update.message.reply_text(f'Вот несколько развлекательных мест для вас:\n'
-                                        f' ~ {entert_lst[0]}\n'
-                                        f' ~ {entert_lst[1]}\n'
-                                        f' ~ {entert_lst[3]}\n'
-                                        f' ~ {entert_lst[4]}\n'
-                                        f' ~ {entert_lst[5]}\n')
+                                        f' ~ {entert_lst[0][0]}\n'
+                                        f'  {entert_lst[0][-1]}\n'
+                                        f' ~ {entert_lst[1][0]}\n'
+                                        f'  {entert_lst[1][-1]}\n'
+                                        f' ~ {entert_lst[2][0]}\n'
+                                        f'  {entert_lst[2][-1]}\n')
+
     elif weather == 'природа':
-        nature_lst = list(get_nuturel_info(m[0], m[1]))
+        nature_lst = list(get_nuturel_info(locat[0], locat[-1]))
         print(nature_lst)
         await update.message.reply_text(f'Вот несколько мест, связанных с природой, для вас:\n'
                                         f' ~ {nature_lst[0]}\n'
                                         f' ~ {nature_lst[1]}\n'
                                         f' ~ {nature_lst[3]}\n')
     elif weather == 'еда':
-        eat_lst = list(get_food_info(m[0], m[1]))
+        eat_lst = list(get_food_info(locat[0], locat[-1]))
         await update.message.reply_text(f'Вот несколько мест, где вы сможете поесть:\n'
                                         f' ~ {eat_lst[0]}\n'
                                         f' ~ {eat_lst[1]}\n'
@@ -157,7 +155,7 @@ async def second_response(update, context):
                                         f' ~ {eat_lst[4]}\n'
                                         f' ~ {eat_lst[5]}\n')
     elif weather == 'культура':
-        cultur_lst = list(get_culture_info(m[0], m[1]))
+        cultur_lst = list(get_culture_info(locat[0], locat[-1]))
         await update.message.reply_text(f'Вот несколько мест, где вы сможете поесть:\n'
                                         f' ~ {cultur_lst[0]}\n'
                                         f' ~ {cultur_lst[1]}\n'
@@ -170,7 +168,8 @@ async def second_response(update, context):
             "Вы ввели не то, что нужно. Попробуйте ещё раз!")
     logger.info(weather)
     if flag:
-        await update.message.reply_text("Если вы нашли то, что вы хотели то введите команду /stop.", reply_markup=markup1)
+        await update.message.reply_text("Если вы нашли то, что вы хотели то введите команду /stop.",
+                                        reply_markup=markup1)
 
 
 async def stop(update, context):
@@ -190,9 +189,7 @@ async def help(update, context):
         "Я бот справочник.")
 
 
-async def address(update, context):
-    await update.message.reply_text(
-        "Адрес: г. Санкт-Петербург, Жемчужная плаза")
+
 
 
 def main():
@@ -214,7 +211,6 @@ def main():
     application = Application.builder().token(BOT_TOKEN).build()
     application.add_handler(conv_handler)
     application.add_handler(MessageHandler(None, receive_location))
-    application.add_handler(CommandHandler("address", address))
     application.add_handler(CommandHandler("help", help))
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("close", close_keyboard))
